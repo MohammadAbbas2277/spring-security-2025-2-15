c56ae9960c282750b5f5693f80a794df18a3a5be
Mohammad Abbas <msaabbas6@gmail.com>
2025-02-15 12:37:32 +0100

    start

---
+++ src/main/java/com/java24/spring_securitity_demo/SpringSecuritityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java24.spring_securitity_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java24/spring_securitity_demo/SpringSecuritityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java24.spring_securitity_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
